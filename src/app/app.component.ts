f3ce98712e394f1fe66d9989de2348dae0ee23cb
Krepssier <krepssier1983@gmail.com>
2024-08-20 14:30:04 +0200

    binding en el app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases-02';
+  public title: string = 'Hola Mundo';
+  public counter: number = 10;
+
 }
